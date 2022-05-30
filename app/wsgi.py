33150b275e08150609bf8b5d1ed03be7048ca493
Riley Thompson <jr3thomp@edu.uwaterloo.ca>
2022-05-30 02:01:49 -0400

    mv wsgi

--- app/wsgi.py
+++
@@ -1,6 +0,0 @@
-from app.server import app
-import os
- 
-if __name__ == "__main__":
-        print('GOT TO HERE')
-        app.run(port = int(os.environ.get('PORT', 17995)))
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app.server import app
+import os
+ 
+if __name__ == "__main__":
+    print('GOT TO HERE')
+    app.run(port = int(os.environ.get('PORT', 17995)))
