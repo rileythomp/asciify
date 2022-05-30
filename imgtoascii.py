7aee485cd4b95345a8b26e577b949949eedd8c20
Riley Thompson <jr3thomp@edu.uwaterloo.ca>
2022-05-29 20:40:22 -0400

    refactor

--- imgtoascii.py
+++ imgtoascii.py
@@ -1,29 +1,45 @@
 from PIL import Image
 import math
 
-def ImgToAscii(imgPath) -> str:
-    chars = '.\'`^",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+Chars = '.\'`^",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+CharsLen = len(Chars)
 
+
+def brightnessToChar(brightness: float) -> str:
+    scaled_brightness = brightness/256
+    char_index = math.floor(scaled_brightness * CharsLen)
+    if char_index >= CharsLen:
+        print('char index out of bounds')
+        print(scaled_brightness, char_index, CharsLen)
+        return ''
+    char = Chars[char_index]
+    return char
+
+def getAsciiDimensions(imgPath: str):
     img = Image.open(imgPath)
     img_width = 100
     width_percent = (img_width/float(img.size[0]))
     img_height = int((float(img.size[1])*float(width_percent)))
     img = img.resize((img_width,img_height), Image.Resampling.LANCZOS)
     width, height = img.size
+    return img, width, height
 
+def ImgToAscii(imgPath) -> str:
+    img, width, height = getAsciiDimensions(imgPath)
     art = ''
     for y in range(height):
         for x in range(width):
             r, g, b = img.getpixel((x, y))
-            brightness = (r+g+b)/3
-            scaled_brightness = brightness/256
-            char_index = math.floor(scaled_brightness * len(chars))
-            if char_index >= len(chars):
-                print('char index out of bounds')
-                print(scaled_brightness, char_index, len(chars))
-                return ''
-            char = chars[char_index]
+            # luminosity
+            brightness =  0.2126*r + 0.7152*g + 0.0722*b
+            # brightness = (r+g+b)/3
+            # lightness
+            # brightness = (max(r, g, b)+min(r, g, b)) / 2
+            # luminosity perceived 1
+            # brightness = 0.299*r + 0.587*g + 0.114*b
+            # luminosity perceived 2
+            # brightness = math.sqrt( 0.299*r*r + 0.587*g*g + 0.114*b*b )
+            char = brightnessToChar(brightness)
             art += char*2
         art += '\n';
-
     return art
--- webcam.py
+++ webcam.py
@@ -2,25 +2,34 @@ import cv2 as cv
 from imgtoascii import ImgToAscii
 import os
 
+FrameName = 'frame.jpg'
+
 # run watch -n 0.1 cat asciiart.txt in a separate terminal to see output
 def strToFile(str: str, filename: str):
     file = open(filename, 'w')
     file.write(str)
     file.close()
 
-frameName = 'frame.jpg'
+def outputAsciiArt(art: str, filename: str):
+    print(art)
+    strToFile(art, filename)
+
+
 
 cam = cv.VideoCapture(0)
 print('press q to stop webcam')
 while (True):
     retval, frame = cam.read()
-    cv.imshow('Ascii Art', frame)
-    cv.imwrite(frameName, frame)
-    asciiart = ImgToAscii(frameName)
-    print(asciiart)
-    strToFile(asciiart, 'asciiart.txt')
-    os.remove(frameName)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    if retval:
+        cv.imshow('Ascii Art', frame)
+        cv.imwrite(FrameName, frame)
+        asciiart = ImgToAscii(FrameName)
+        outputAsciiArt(asciiart, 'asciiart.txt')
+        os.remove(FrameName)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        print('error recording webcam frame')
         break
 
 cam.release()
